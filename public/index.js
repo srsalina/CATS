f4cfc6ca9f84946e928665b72a05a8c6cc1f4e85
ava.luxe <ajrluxe@gmail.com>
2023-10-04 18:29:53 -0500

    onload, cat initialization, fetch skeleton

--- public/index.js
+++ public/index.js
@@ -1,48 +1,83 @@
-// * _____________________phase 1 _____________________*
-window.onload = () => {
 
+// * _____________________phase 1 _____________________*
+const initializePage = () => {
+
+
+   const firstContainer = document.createElement('section');
+   firstContainer.setAttribute("class", "firstContainer");
+
+
+
+   document.body.appendChild(firstContainer);
+
+   mainContent();
+   voteContainer();
+   commentsContainer();
+
+
+   if (localStorage.url) {
+      document.querySelector("img").src = localStorage.url;
+      document.querySelector(".score").innerHTML = localStorage.score;
+      commentsContainer(JSON.parse(localStorage.comments))
+   } else {
+
+      fetchImage();
+   }
+
+
+
+}
+
+const mainContent = () => {
    // insert div container for h1 and img connected to body
    // console.log('TESTING TESTING!');
+
    const centerContent = document.createElement('div');
    centerContent.setAttribute("id", "firstHeader")
    document.body.appendChild(centerContent);
 
-//1st part: insert h1
+   //1st part: insert h1
    const firstHeader = document.createElement('h1');
    firstHeader.innerText = "Kitten Pic";
    firstHeader.setAttribute("class", "firstHeader");
    centerContent.appendChild(firstHeader);
 
-
    //2nd part: insert img
 
 
-   createKittenContainer();
 
 
 
+
+}
 
 
    // * _____________________phase 2 _____________________*
 
 // insert div container to hold under img called voteContainer connected to body and UNDER the first container
 
-const voteContainer = document.createElement
+const voteContainer = () => {
 
-//1st part: insert span section
 
-//2nd part: insert voteButtons container div
+
+   //1st part: insert span section
+
+   //2nd part: insert voteButtons container div
    // -  button class upVote
    // -  button class downVote
 
 
-
+}
 // ____________________________________________________________
 // insert commentsContainer div connected to body UNDER the second container
+
+const commentsContainer = () => {
+
    // -  div = comments-main
    //    -  label(?), input (placeholder = Add a comment...), button
    // -  div = comments-box
 
+}
 
 
 
@@ -54,6 +89,23 @@ const voteContainer = document.createElement
 
    // * _____________________phase 4 _____________________*
 
+const fetchImage = async () => {
+   try {
+      const catRes = await fetch("https://api.thecatapi.com/v1/images/search");
+      const catData = await catRes.json();
+   } catch (e) {
+      console.log("Failed to fetch image", e);
+
+   }
+}
+
+
+
+
+
+window.onload = async () => {
+
+   initializePage();
 
 
 
--- public/interactive/kitten.js
+++
@@ -1,4 +0,0 @@
-//create kitten container
-// * don't forget to export
-
-//get kitten image
